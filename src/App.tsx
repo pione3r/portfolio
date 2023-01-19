4885fc68759c9696087e6882ebbf6680d32a0e08
yongjunhyun <dev2diver@gmail.com>
2023-01-19 15:19:16 +0900

    style: index.css init_v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Test = styled.div``;
+const Container = styled.div``;
+
+const Header = styled.header`
+  border: 2px solid limegreen;
+`;
+
+const Main = styled.main`
+  border: 2px solid limegreen;
+`;
 
 function App() {
-  return <Test>Bye</Test>;
+  return (
+    <Container>
+      <Header>이건 헤더다</Header>
+      <Main>이건 메인이다</Main>
+    </Container>
+  );
 }
 
 export default App;
