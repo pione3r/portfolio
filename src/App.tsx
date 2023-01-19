fba5a4c8c1f26d41bde531c06636d713538b4473
yongjunhyun <dev2diver@gmail.com>
2023-01-19 22:33:30 +0900

    feat: 헤더 컴포넌트 구현_2
    - index.css에 global style variable 구현
    - 헤더 메인 로고 / navbar hover style 추가
    - 반응형 구현 / 모바일은 드롭다운 nav
    - 라이트 모드 / 다크모드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const Container = styled.div``;
 
@@ -9,17 +9,18 @@ const Header = styled.header`
 
   padding: 16px 32px;
 
-  border: 2px solid limegreen;
+  position: sticky;
+  top: 0;
 `;
 
-const HeaderLogo = styled.div`
+const HeaderLogo = styled.a`
   font-weight: var(--base-text-weight-bold);
   font-size: var(--base-text-size-large);
 
-  cursor: pointer;
+  color: var(--header-logo-text-color);
 
   &:hover {
-    color: var(--base-text-color-hover);
+    color: var(--header-logo-text-color-hover);
   }
 `;
 
@@ -28,26 +29,108 @@ const Nav = styled.nav`
   gap: 16px;
 
   margin-left: auto;
+
+  // 세로모드 모바일 디바이스 (가로 해상도가 415px 보다 작은 화면에 적용)
+  @media (max-width: 414px) {
+    flex-direction: column;
+
+    position: absolute;
+    top: 70px;
+    right: 20px;
+
+    display: ${(props) =>
+      props.className === 'dropdown-toggle-active' ? '' : 'none'};
+  }
 `;
 
-const NavItem = styled.div`
+const underline = keyframes`
+  0% {
+    width: 0%;
+  }
+
+  100% {
+    width: 100%;
+  }
+`;
+
+const NavItem = styled.a`
+  position: relative;
+
+  width: fit-content;
+
   font-weight: var(--base-text-weight-semibold);
   font-size: var(--base-text-size-medium);
 
-  cursor: pointer;
+  color: var(--navbar-item-text-color);
+
+  &:hover {
+    color: var(--navbar-item-text-color-hover);
+  }
+
+  &:hover::after {
+    content: '';
+    position: absolute;
+    left: 0px;
+    bottom: -2px;
+
+    width: 100%;
+    height: 3px;
+
+    background: red;
+
+    animation: ${underline} 0.5s;
+  }
+`;
+
+const DropDownButtonContainer = styled.div`
+  display: none;
+
+  // 세로모드 모바일 디바이스 (가로 해상도가 415px 보다 작은 화면에 적용)
+  @media (max-width: 414px) {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+
+    margin-left: auto;
+
+    &:active {
+      background-color: var(--dropdown-color-hover);
+    }
+  }
+`;
+
+const DropDownButtonItem = styled.div`
+  width: 20px;
+  height: 5px;
+
+  border-radius: 6px;
+
+  background-color: var(--dropdown-color);
 `;
 
 const DarkmodeToggle = styled.button``;
 
 function App() {
+  const [dropDownToggleClicked, setDropDownToggleClicked] = useState(false);
   return (
     <Container>
       <Header>
-        <HeaderLogo>DevDive</HeaderLogo>
-        <Nav>
-          <NavItem>About Me</NavItem>
-          <NavItem>Skills</NavItem>
-          <NavItem>Projects</NavItem>
+        <HeaderLogo href="#">DevDive</HeaderLogo>
+        <DropDownButtonContainer
+          onClick={() => {
+            setDropDownToggleClicked((prev) => !prev);
+          }}
+        >
+          <DropDownButtonItem></DropDownButtonItem>
+          <DropDownButtonItem></DropDownButtonItem>
+          <DropDownButtonItem></DropDownButtonItem>
+        </DropDownButtonContainer>
+        <Nav
+          className={`${dropDownToggleClicked ? 'dropdown-toggle-active' : ''}`}
+        >
+          <NavItem href="#About Me">About Me</NavItem>
+          <NavItem href="#Skills">Skills</NavItem>
+          <NavItem href="#Projects">Projects</NavItem>
         </Nav>
       </Header>
       <DarkmodeToggle
@@ -60,8 +143,6 @@ function App() {
           if (theme === 'light') {
             document.body.setAttribute('data-theme', 'dark');
           }
-
-          console.log(window.matchMedia('(prefers-color-scheme)'));
         }}
       >
         다크 모드 토글 버튼
