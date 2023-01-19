24be427f06d0f1901460b85c2a4662ea1d8d3576
yongjunhyun <dev2diver@gmail.com>
2023-01-19 18:54:38 +0900

    feat: 헤더 컴포넌트 구현 / 다크모드 토글 구현
    - 헤더 로고 / 네비게이션 초기 구현
    - 다크 모드 토글 버튼 초기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  height: 100vh;
-  border: 2px solid limegreen;
-`;
+const Container = styled.div``;
 
 const Header = styled.header`
+  display: flex;
+  align-items: center;
+
+  padding: 16px 32px;
+
   border: 2px solid limegreen;
 `;
 
-const Main = styled.main`
-  border: 2px solid limegreen;
+const HeaderLogo = styled.div`
+  font-weight: var(--base-text-weight-bold);
+  font-size: var(--base-text-size-large);
+
+  cursor: pointer;
+
+  &:hover {
+    color: var(--base-text-color-hover);
+  }
 `;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 16px;
+
+  margin-left: auto;
+`;
+
+const NavItem = styled.div`
+  font-weight: var(--base-text-weight-semibold);
+  font-size: var(--base-text-size-medium);
+
+  cursor: pointer;
+`;
+
+const DarkmodeToggle = styled.button``;
 
 function App() {
   return (
     <Container>
-      <Header>이건 헤더다</Header>
-      <Main>이건 메인이다</Main>
+      <Header>
+        <HeaderLogo>DevDive</HeaderLogo>
+        <Nav>
+          <NavItem>About Me</NavItem>
+          <NavItem>Skills</NavItem>
+          <NavItem>Projects</NavItem>
+        </Nav>
+      </Header>
+      <DarkmodeToggle
+        onClick={() => {
+          const theme = document.body.getAttribute('data-theme');
+          if (theme === 'dark') {
+            document.body.setAttribute('data-theme', 'light');
+          }
+
+          if (theme === 'light') {
+            document.body.setAttribute('data-theme', 'dark');
+          }
+
+          console.log(window.matchMedia('(prefers-color-scheme)'));
+        }}
+      >
+        다크 모드 토글 버튼
+      </DarkmodeToggle>
     </Container>
   );
 }
